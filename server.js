ecaab2bad9fe55be68bf9d14cd93288b99bcf19e
HabibaAbouelyazeed <48604366+HabibaAbouelyazeed@users.noreply.github.com>
2021-12-25 05:06:18 +0200

    update sever file

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+var bodyParser = require('body-parser');
+var cors = require('cors')
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(3200, function(){
+    console.log("server runs on port 3200");
+    //console.log('CORS-enabled web server listening on port 3200');
+});
+
+
+app.get('/getProjData',(req,res)=>{
+    res.send(projectData);
+});
+
+
+app.post('/postProjData',(req,res)=>{
+    const data = req.body;
+    projectData = data;
+    res.send()
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,11 @@
 /* Global Variables */
 
+const API_key = '64a12144e01e9808d2ff09f7d9e1589e';
+
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() +'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
